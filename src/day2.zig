a4f69a58e03f4478d99aced54b6eef3028b90d00
Sykotes <sykoted@gmail.com>
2023-12-02 18:58:15 +0000

    day2 part1 progress is being made

--- src/day2.zig
+++ src/day2.zig
@@ -13,30 +13,51 @@ const Sets = struct {
 };
 
 fn getSets(line: []const u8) !Sets {
-    var sets_start_index = std.mem.indexOfAny(u8, line, ": ");
-    var sets_in_line = line[sets_start_index.?..];
-    var set_first_index = std.mem.indexOfAny(u8, sets_in_line, "; ");
-    var set_last_index = std.mem.lastIndexOfAny(u8, sets_in_line, "; ");
+    var sets_start_index = std.mem.indexOfScalar(u8, line, ':');
+    std.log.info("{}\n", .{sets_start_index.?});
+    var sets_in_line = line[sets_start_index.? + 2 ..];
+    std.log.info("{s}", .{sets_in_line});
+    var set_first_index = std.mem.indexOfScalar(u8, sets_in_line, ';');
+    var set_last_index = std.mem.lastIndexOfScalar(u8, sets_in_line, ';');
 
     const sets = Sets{
         .set_1 = sets_in_line[0..set_first_index.?],
-        .set_2 = sets_in_line[set_first_index.?..set_last_index.?],
-        .set_3 = sets_in_line[set_last_index.?..],
+        .set_2 = sets_in_line[set_first_index.? + 2 .. set_last_index.?],
+        .set_3 = sets_in_line[set_last_index.? + 2 ..],
     };
+    std.log.info("{}\n", .{set_first_index.?});
+    std.log.info("{}\n", .{set_last_index.?});
+
+    std.log.info("{s}\n", .{sets.set_1});
+    std.log.info("{s}\n", .{sets.set_2});
+    std.log.info("{s}\n", .{sets.set_3});
 
     return sets;
 }
 
-fn getValuesFromSet(sets: Sets) !rgb {
-    var split_set_1 = std.mem.split(u8, sets.set_1, ", ");
-    _ = split_set_1;
-    // a set is "10 red, 19 blue, 3 green"
-
-    return rgb{
-        .red = 10,
-        .green = 11,
-        .blue = 12,
+fn getValuesFromSet(set: []const u8) !rgb {
+    var set_rgb = rgb{
+        .red = 0,
+        .green = 0,
+        .blue = 0,
     };
+
+    var split_set = std.mem.split(u8, set, ", ");
+    while (split_set.next()) |colour| {
+        _ = colour;
+        // const space_index = std.mem.indexOfScalar(u8, colour, ' ');
+        // const number_string = colour[0..space_index.?];
+        // const number = try std.fmt.parseInt(u16, number_string, 10);
+
+        // const first_letter_of_colour = colour[space_index.? + 1];
+        // switch (first_letter_of_colour) {
+        //     'r' => set_rgb.red = number,
+        //     'g' => set_rgb.green = number,
+        //     'b' => set_rgb.blue = number,
+        //     else => {},
+        // }
+    }
+    return set_rgb;
 }
 
 fn isValidGame(set_total: rgb) bool {
@@ -48,22 +69,36 @@ fn isValidGame(set_total: rgb) bool {
 }
 
 pub fn part1() !void {
-    const file = try std.fs.cwd().openFile("input_files/day2.txt", .{});
+    const file = try std.fs.cwd().openFile("input_files/day2_test.txt", .{});
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     const in_stream = buf_reader.reader();
 
     var game_number: u16 = 0;
-    _ = game_number;
+    var sets = Sets{
+        .set_1 = undefined,
+        .set_2 = undefined,
+        .set_3 = undefined,
+    };
+
+    var set_1_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
+    var set_2_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
+    _ = set_2_values;
+    var set_3_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
+    _ = set_3_values;
     var set_total: rgb = undefined;
     _ = set_total;
 
     var sum: u16 = 0;
+    _ = sum;
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
+        game_number += 1;
+        sets = try getSets(line);
+
+        set_1_values = try getValuesFromSet(sets.set_1);
     }
-    std.debug.print("Day2 Part1: {}", .{sum});
+    // std.debug.print("Day2 Part1: {}\n", .{sum});
 }
--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const day1 = @import("day1.zig");
 const day2 = @import("day2.zig");
 
 pub fn main() !void {
-    try day1.part1Improved();
-    try day1.part2();
+    // try day1.part1Improved();
+    // try day1.part2();
     try day2.part1();
 }
