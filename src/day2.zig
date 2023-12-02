b6d5b0aa344b251ed241f2a2190a2bd00ee4acf2
Sykotes <sykoted@gmail.com>
2023-12-02 20:03:07 +0000

    day2 part1 i clear;y dont know what im doing

--- src/day2.zig
+++ src/day2.zig
@@ -1,66 +1,51 @@
 const std = @import("std");
 
-const rgb = struct {
+const RGB = struct {
     red: u16,
     green: u16,
     blue: u16,
 };
 
-const Sets = struct {
-    set_1: []const u8,
-    set_2: []const u8,
-    set_3: []const u8,
-};
-
-fn getSets(line: []const u8) !Sets {
+fn getSets(line: []const u8) !std.ArrayList(RGB) {
     var sets_start_index = std.mem.indexOfScalar(u8, line, ':');
-    std.log.info("{}\n", .{sets_start_index.?});
     var sets_in_line = line[sets_start_index.? + 2 ..];
-    std.log.info("{s}", .{sets_in_line});
-    var set_first_index = std.mem.indexOfScalar(u8, sets_in_line, ';');
-    var set_last_index = std.mem.lastIndexOfScalar(u8, sets_in_line, ';');
 
-    const sets = Sets{
-        .set_1 = sets_in_line[0..set_first_index.?],
-        .set_2 = sets_in_line[set_first_index.? + 2 .. set_last_index.?],
-        .set_3 = sets_in_line[set_last_index.? + 2 ..],
-    };
-    std.log.info("{}\n", .{set_first_index.?});
-    std.log.info("{}\n", .{set_last_index.?});
+    const number_of_sets = std.mem.count(u8, sets_in_line, "; ") + 1;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var set_array = try std.ArrayList(RGB).initCapacity(gpa.allocator(), number_of_sets);
+    errdefer set_array.deinit();
 
-    std.log.info("{s}\n", .{sets.set_1});
-    std.log.info("{s}\n", .{sets.set_2});
-    std.log.info("{s}\n", .{sets.set_3});
+    var split_sets = std.mem.split(u8, sets_in_line, "; ");
+    while (split_sets.next()) |set| {
+        try set_array.append(set);
+    }
 
-    return sets;
+    return set_array;
 }
 
-fn getValuesFromSet(set: []const u8) !rgb {
-    var set_rgb = rgb{
+fn getTotalsFromSet(set_array: std.ArrayList(RGB)) !RGB {
+    var sets_total = RGB{
         .red = 0,
         .green = 0,
         .blue = 0,
     };
+    for (set_array) |set| {
+        const space_index = std.mem.indexOfScalar(u8, set, ' ');
+        const number_string = set[0..space_index.?];
+        const number = try std.fmt.parseInt(u16, number_string, 10);
 
-    var split_set = std.mem.split(u8, set, ", ");
-    while (split_set.next()) |colour| {
-        _ = colour;
-        // const space_index = std.mem.indexOfScalar(u8, colour, ' ');
-        // const number_string = colour[0..space_index.?];
-        // const number = try std.fmt.parseInt(u16, number_string, 10);
-
-        // const first_letter_of_colour = colour[space_index.? + 1];
-        // switch (first_letter_of_colour) {
-        //     'r' => set_rgb.red = number,
-        //     'g' => set_rgb.green = number,
-        //     'b' => set_rgb.blue = number,
-        //     else => {},
-        // }
+        const first_letter_of_colour = set[space_index.? + 1];
+        switch (first_letter_of_colour) {
+            'r' => sets_total.red += number,
+            'g' => sets_total.green += number,
+            'b' => sets_total.blue += number,
+            else => {},
+        }
     }
-    return set_rgb;
+    return sets_total;
 }
 
-fn isValidGame(set_total: rgb) bool {
+fn isValidGame(set_total: RGB) bool {
     if (set_total.red <= 12) return false;
     if (set_total.green <= 13) return false;
     if (set_total.blue <= 14) return false;
@@ -76,29 +61,18 @@ pub fn part1() !void {
     const in_stream = buf_reader.reader();
 
     var game_number: u16 = 0;
-    var sets = Sets{
-        .set_1 = undefined,
-        .set_2 = undefined,
-        .set_3 = undefined,
-    };
-
-    var set_1_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
-    var set_2_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
-    _ = set_2_values;
-    var set_3_values = rgb{ .red = undefined, .green = undefined, .blue = undefined };
-    _ = set_3_values;
-    var set_total: rgb = undefined;
-    _ = set_total;
-
+    var sets: std.ArrayList(RGB) = undefined;
+    var sets_total: RGB = undefined;
     var sum: u16 = 0;
-    _ = sum;
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         game_number += 1;
-        sets = try getSets(line);
 
-        set_1_values = try getValuesFromSet(sets.set_1);
+        sets = try getSets(line);
+        sets_total = try getTotalsFromSet(sets);
+
+        if (isValidGame(sets_total)) sum += game_number;
     }
-    // std.debug.print("Day2 Part1: {}\n", .{sum});
+    std.debug.print("Day2 Part1: {}\n", .{sum});
 }
