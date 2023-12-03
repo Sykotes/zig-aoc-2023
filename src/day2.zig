70ed8152bd79c2f80cdcef1b22b2b419a8329c14
Sykotes <sykoted@gmail.com>
2023-12-03 17:20:03 +0000

    day2 part1 i is stupid but it work

--- src/day2.zig
+++ src/day2.zig
@@ -18,60 +18,51 @@ fn getSets(sets_string: []const u8) !std.ArrayList(Set) {
     var list_of_sets: std.ArrayList(Set) = std.ArrayList(Set).init(std.heap.page_allocator);
 
     while (sets_string_iter.next()) |set_string| {
-        var set = Set{
-            .r = 0,
-            .g = 0,
-            .b = 0,
-        };
+        var item_string_iter = std.mem.split(u8, set_string, ", ");
+        while (item_string_iter.next()) |item| {
+            var set = Set{
+                .r = 0,
+                .g = 0,
+                .b = 0,
+            };
 
-        const index_of_space_maybe = std.mem.indexOfScalar(u8, set_string, ' ');
-        const string_of_number = set_string[0..index_of_space_maybe.?];
-        const number = try std.fmt.parseInt(u8, string_of_number, 10);
+            const index_of_space_maybe = std.mem.indexOfScalar(u8, item, ' ');
+            const string_of_number = item[0..index_of_space_maybe.?];
+            const number = try std.fmt.parseInt(u8, string_of_number, 10);
 
-        const first_letter_of_colour = set_string[index_of_space_maybe.? + 1];
-        switch (first_letter_of_colour) {
-            'r' => set.r = number,
-            'g' => set.b = number,
-            'b' => set.b = number,
-            else => {},
+            const first_letter_of_colour = item[index_of_space_maybe.? + 1];
+            switch (first_letter_of_colour) {
+                'r' => set.r = number,
+                'g' => set.g = number,
+                'b' => set.b = number,
+                else => {},
+            }
+            try list_of_sets.append(set);
         }
-
-        try list_of_sets.append(set);
     }
 
     return list_of_sets;
 }
 
-fn addUpSetValues(list_of_sets: std.ArrayList(Set)) Set {
-    var total_set = Set{
-        .r = 0,
-        .g = 0,
-        .b = 0,
-    };
+fn isValidGame(list_of_sets: std.ArrayList(Set)) bool {
     for (list_of_sets.items) |set| {
-        total_set.r += set.r;
-        total_set.g += set.g;
-        total_set.b += set.b;
+        if (set.r > 12) return false;
+        if (set.g > 13) return false;
+        if (set.b > 14) return false;
     }
-
-    return total_set;
-}
-
-fn isValidGame(total_set: Set) bool {
-    if (total_set.r > 12) return false;
-    if (total_set.g > 13) return false;
-    if (total_set.b > 14) return false;
     return true;
 }
 
 pub fn part1() !void {
-    const file = try std.fs.cwd().openFile("input_files/day2_test.txt", .{});
+    const file = try std.fs.cwd().openFile("input_files/day2.txt", .{});
     defer file.close();
 
-    var game_id: u8 = 0;
+    var game_id: u16 = 0;
     var list_of_sets: std.ArrayList(Set) = undefined;
     var sets_string: []const u8 = undefined;
     var total_set: Set = undefined;
+    var is_valid_game = false;
+    _ = is_valid_game;
     var sum: u32 = 0;
 
     var buf_reader = std.io.bufferedReader(file.reader());
@@ -83,11 +74,14 @@ pub fn part1() !void {
 
         sets_string = try getSetsString(line);
         list_of_sets = try getSets(sets_string);
-        total_set = addUpSetValues(list_of_sets);
 
-        if (isValidGame(total_set)) {
-            std.debug.print("Game {} is valid!\n", .{game_id});
-            std.debug.print("Total set:\nRed: {}\nGreen {}\nBlue: {}\n", .{ total_set.r, total_set.g, total_set.b });
+        if (game_id == 1) {
+            std.debug.print("Total set:\nRed: {}\nGreen: {}\nBlue: {}\n", .{ total_set.r, total_set.g, total_set.b });
+        }
+
+        if (isValidGame(list_of_sets)) {
+            std.debug.print("Valid Game ID: {}\n", .{game_id});
+            std.debug.print("Total set:\nRed: {}\nGreen: {}\nBlue: {}\n", .{ total_set.r, total_set.g, total_set.b });
             sum += game_id;
         }
     }
