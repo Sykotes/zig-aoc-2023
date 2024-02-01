189e6c95a6b1f4c0501655d1fcc78e87776e5544
Sykotes <sykoted@gmail.com>
2024-02-01 20:23:43 +0000

    day3 part1 fmt change

--- src/day3.zig
+++ src/day3.zig
@@ -10,24 +10,23 @@ pub fn part1() !void {
     const not_symbols = "0123456789.";
 
     var buf: [1024]u8 = undefined;
-    var previous_line: ?[buf.len]u8 = null;
+    var possible_previous_line: ?[buf.len]u8 = null;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         _ = line;
 
-        if (previous_line == null) {
-            previous_line = buf;
-            continue;
-        }
-        previous_line = buf;
-        const new_line_index = std.mem.indexOfScalar(u8, buf[0..buf.len], '\n');
+        if (possible_previous_line) |previous_line| {
+            const new_line_index = std.mem.indexOfScalar(u8, buf[0..buf.len], '\n');
 
-        std.debug.print("\n\n{s}", .{previous_line.?});
+            std.debug.print("{s}", .{previous_line});
 
-        for (previous_line.?[0..new_line_index.?], 0..) |character, index| {
-            if (std.mem.indexOfScalar(u8, not_symbols, character) == null) {
-                std.debug.print("{}: {c}\n", .{ index, character });
+            for (previous_line[0..new_line_index.?], 0..) |character, index| {
+                if (std.mem.indexOfScalar(u8, not_symbols, character) == null) {
+                    // std.debug.print("{}: {c}\n", .{ index, character });
+                    _ = index;
+                }
             }
         }
+        possible_previous_line = buf;
     }
 }
